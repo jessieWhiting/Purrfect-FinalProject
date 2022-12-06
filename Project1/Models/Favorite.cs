a3ef9762e1475e4ac00f834989e16ee5f6178100
jessieWhiting <109237483+jessieWhiting@users.noreply.github.com>
2022-12-06 14:28:19 -0500

    "added working photos to Additional Info Page"

---
+++ Project1/Models/Favorite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project1.Models;
+
+public partial class Favorite
+{
+    public int FavoriteId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int? CatId { get; set; }
+
+    public virtual Cat? Cat { get; set; }
+
+    public virtual User? User { get; set; }
+}
