6621c3ffe8653b7eeaf56702147b6fc14aea2933
Abilena <abthomas625@gmail.com>
2022-12-14 08:51:55 -0600

    removed string

--- Project1/Models/AdoptionCenterContext.cs
+++ Project1/Models/AdoptionCenterContext.cs
@@ -23,7 +23,7 @@ public partial class AdoptionCenterContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-    => optionsBuilder.UseSqlServer("Data Source=purrpose.database.windows.net;Initial Catalog=AdoptionCenter; User Id=PurrposeProject; Password=C@tsrule!");
+    => optionsBuilder.UseSqlServer("ConnectionStrings:ConnectionString");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
